6cffbe43ce697d4689d3b670dfabe8f314676840
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-04 10:13:22 +0530

    multer added

--- server/index.js
+++ server/index.js
@@ -12,7 +12,35 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-
+// Configuration 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+// File Storage 
+
+const storage = multer.diskStorage({
+    destination : function(req, file, cb) {
+        cb(null, "public/assets")
+    } ,
+    filename : function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({storage});
+
+
+
+
 

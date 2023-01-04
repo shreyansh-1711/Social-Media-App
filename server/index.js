d6f1a9ac3278e773a55790ec3a723401330d46b3
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-04 11:16:12 +0530

    Mongoose connection setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-import { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -21,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+// app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -39,6 +38,17 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage});
+
+// Mongoose Setup 
+const PORT = process.env.PORT || 6001;
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
+
 
 
 

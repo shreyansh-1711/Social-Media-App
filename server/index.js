47ba97b7f518eacacf850ef9519d662f9f05373f
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-04 19:08:18 +0530

    User schema created

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
+import {register} from "./controllers/auth.js";
+
 
 // For path 
 import path from "path";
@@ -25,6 +27,11 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+
+// Routes With FILES 
+app.post("/auth/register", upload.single("picture"), register );
+
 
 // File Storage 
 

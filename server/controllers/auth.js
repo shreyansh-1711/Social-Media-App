ee95b05f5e3f7f8273d1c7707fbbd46eaab3d835
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-05 16:43:03 +0530

    authjs done

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -17,10 +17,24 @@ export const register = async (req, res) => {
             occupation } = req.body;
 
         const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);    
-        const user = await User.findOne({ email });
+        const passwordHash = await bcrypt.hash(password, salt);
 
-        if (user) {
-            return res.status(400).json({ error: 'User already exists' });
-        }
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password : passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile : Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    }catch(err) {
+        res.status(500).json({error: err.message});
     }
+};

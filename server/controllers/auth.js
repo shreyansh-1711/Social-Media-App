76f0659a8b0b86725fcaa1079e96900dea65e7ac
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-05 09:23:45 +0530

    User schema created

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,3 +2,25 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User.js';
+
+
+// Regisrer USer 
+export const register = async (req, res) => {
+    try {
+        const { firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);    
+        const user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+    }

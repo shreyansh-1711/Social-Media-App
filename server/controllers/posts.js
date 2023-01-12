57f0584c06913c46a8347f68fbc2749643149da3
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-13 00:29:24 +0530

    update post  controller created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,18 +4,18 @@ import User from "../models/User.js";
 // Create  
 export const createPost = async (req, res) => {
     try {
-        const { userId , description, picturePath} = req.body;
+        const { userId, description, picturePath } = req.body;
         const user = await User.findById(userId);
         const newPost = new Post({
             userId,
-            firstName : user.firstName,
-            lastName : user.lastName,
-            location : user.location,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
             description,
-            userPicturePath : user.picturePath,
+            userPicturePath: user.picturePath,
             picturePath,
-            likes : {},
-            comments : []
+            likes: {},
+            comments: []
         })
 
         await newPost.save();
@@ -25,7 +25,7 @@ export const createPost = async (req, res) => {
 
 
     } catch (err) {
-        res.status(409).json({ message : err.message });
+        res.status(409).json({ message: err.message });
     }
 }
 
@@ -36,9 +36,9 @@ export const getPosts = async (req, res) => {
     try {
         const post = await Post.find();
         res.status(200).json(post);
-        
+
     } catch (error) {
-        res.status(404).json({ message : err.message });
+        res.status(404).json({ message: err.message });
     }
 }
 
@@ -47,9 +47,39 @@ export const getUserPosts = async (req, res) => {
         const { userId } = req.params;
         const post = await Post.find({ userId });
         res.status(200).json(post);
-        
-    } 
-     catch (error) {
-        res.status(404).json({ message : err.message });
+
+    }
+    catch (error) {
+        res.status(404).json({ message: err.message });
+    }
+}
+
+
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+        if (isLiked) {
+            post.likes.delete(userId);
+
+        }
+        else {
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }
+        );
+
+
+
+        res.status(200).json(updatedPost);
+
+    } catch (error) {
+        res.status(404).json({ message: err.message });
     }
 }

e59711708889510f28b8520a9f4e81eb9aa8cf66
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-12 22:27:37 +0530

    Create Post controller created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,37 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+// Create  
+export const createPost = async (req, res) => {
+    try {
+        const { userId , description, picturePath} = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName : user.firstName,
+            lastName : user.lastName,
+            location : user.location,
+            description,
+            userPicturePath : user.picturePath,
+            picturePath,
+            likes : {},
+            comments : []
+        })
+
+        await newPost.save();
+
+        const post = await Post.find();
+        res.status(201).json(post);
+
+
+    } catch (err) {
+        res.status(409).json(err);
+    }
+}
+
+
+
+// Read
+export const getPosts = async (req, res) => {
+
+}

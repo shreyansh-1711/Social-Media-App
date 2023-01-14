e5b165687033ab17d81acac35f89f79f2790e855
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-14 23:15:03 +0530

    navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import {
+  IconButton,
+  Box,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+
+import { Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notification,
+    Help,
+    Close
+ } from "@mui/icons-material";
+
+ import { useDispatch, useSelector } from "react-redux";
+
 const Navbar = () => {
-    return (<div>Navbar</div>)
-}
+  return <div>Navbar</div>;
+};
 
 export default Navbar;

31fa3ad7e01df9b855e6be54dad270bbdf25a27b
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-16 22:26:18 +0530

    navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -32,6 +32,8 @@ const Navbar = () => {
   const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
+
+  
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;

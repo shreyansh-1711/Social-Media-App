dc3975bcc15757a0b58a256980a984378af075c2
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-20 19:59:36 +0530

    login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,11 @@
+import { Box } from "@mui/system";
+import { Typography, useTheme, useMediaQuery } from "@mui/material";
+
+
 const LoginPage = () => {
+
+    const theme = useTheme()
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
     return (<div>loginpage</div>)
 }
 export default LoginPage;

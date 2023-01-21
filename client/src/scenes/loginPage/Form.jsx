c1bdea58d8ae81ae132a671bed81165a28af91ea
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-21 10:32:34 +0530

    Formik

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -17,12 +17,72 @@ import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 
 const registerSchema = yup.object().shape({
-    firstName:
-    lastName:
-    email:
-    password:
-    occupation:
-    picture:
-    
-})
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("Invalid Email").required("required"),
+    password: yup.string().required("required"),
+    location: yup.string().required("required"),
+    occupation: yup.string().required("required"),
+    picture: yup.string().required("required"),
+
+});
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email("Invalid Email").required("required"),
+    password: yup.string().required("required"),
+});
+
+const initialValueRegister = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    location:"",
+    occupation: "",
+    picture: "",
+}
+
+
+const initialValuesLogin = {
+    email: "",
+    password: "",
+}
+
+const Form = () => {
+    const [pageType , setPageType] = useState("login");
+    const { palette } = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("(min-width: 600px)"); 
+    const isLogin = pageType === "login";
+    const isRegister = pageType === "register";
+
+    const handleFormSubmit = async (values, onsubmitProps) => {
+        return(
+            <Formik
+            onSubmit={handleFormSubmit}
+            initialValues= {isLogin ? initialValuesLogin : initialValuesRegister}
+            validationSchema = {isLogin ? loginSchema : registerSchema} 
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleBlur,
+                    handleChange,
+                    handleSubmit,
+                    setFeildValue,
+                    resetForm,
+                })=> (
+                    
+                )}
+
+            </Formik>
+        )
+    }
+
+}        
+
+export default Form;
+
 

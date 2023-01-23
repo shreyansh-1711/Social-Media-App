5f7f4dd28d5b17b96c5969904a1cd62c28f303d1
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-23 23:39:32 +0530

    update

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -56,7 +56,7 @@ const UserWidget = ({userId, picturePath}) => {
             <FlexBetween
             gap="0.5rem"
             pb="1.1rem"
-            
+            onClick={()=>navigate(`/profile/${userId}`)}
             >
             </FlexBetween>
         </WidgetWrapper>

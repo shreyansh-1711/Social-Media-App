b568d7bb23de3ce2e779c9db0657658a03983f1a
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-24 21:34:49 +0530

    Final push

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SocialMedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween

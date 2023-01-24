38bce18760534264fff67574619e59b67e5924dd
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-24 18:42:46 +0530

    Completed

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,19 +1,17 @@
-import { Box } from "@mui/system";
+import { Box } from "@mui/material";
 
-const UserImage = ({ image , size="60px"}) => {
-    <Box
-        width={size}
-        height={size}
-    >
-        <img
-        style={{objectFit: "cover", borderRadius: "50%"}}
+const UserImage = ({ image, size = "60px" }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
         alt="user"
-        src={`https://localhost:3001/assets/${image}`}
-        /> 
-
+        src={`http://localhost:3001/assets/${image}`}
+      />
     </Box>
-}
+  );
+};
 
 export default UserImage;
--- server/index.js
+++ server/index.js
@@ -63,9 +63,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
-
